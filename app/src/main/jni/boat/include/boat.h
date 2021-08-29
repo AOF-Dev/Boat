b1355376598c3e1ee20fd109d5befd2407784976
CosineMath <boat_jvm@qq.com>
2021-08-30 00:18:52 +0800

    Split boat.h
    Update list of public functions. Insert void into non-parameter functions
    because boat.h might be used in C89 codes.

--- app/src/main/jni/boat/include/boat.h
+++ app/src/main/jni/boat/include/boat.h
@@ -2,17 +2,15 @@
 #define BOAT_H
 
 #include <android/native_window.h>
+#include <boat_event.h>
 
-ANativeWindow* boatGetNativeWindow();
-void* boatGetNativeDisplay();
-void boatSetCurrentEventProcessor(BoatEventProcessor);
-void boatGetCurrentEvent(BoatInputEvent*);
-void boatGetCurrentEvent(BoatInputEvent*);
-int boatGetEventPipeFd();
-void boatSetCursorMode(int);
-void boatSetCursorPos(int, int);
-void boatSetPrimaryClipString(const char*);
-const char* boatGetPrimaryClipString();
+ANativeWindow* boatGetNativeWindow(void);
+int boatWaitForEvent(int timeout);
+int boatPollEvent(BoatEvent* event);
+int boatGetEventFd(void);
+void boatSetCursorMode(int mode);
+void boatSetPrimaryClipString(const char* string);
+const char* boatGetPrimaryClipString(void);
 
 #endif
 
