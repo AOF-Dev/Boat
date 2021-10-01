03bd6625f702fc8b446f8151d16b8ab486e8e523
CosineMath <boat_jvm@qq.com>
2021-10-01 14:29:55 +0800

    Delete views and layouts specific for Minecraft in BoatActivity.java. Make BoatActivity abstract.

--- app/src/main/java/cosine/boat/BoatActivity.java
+++ app/src/main/java/cosine/boat/BoatActivity.java
@@ -3,44 +3,45 @@ package cosine.boat;
 import android.view.MotionEvent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.widget.PopupWindow;
+import android.os.SystemClock;
 import android.widget.RelativeLayout;
-import android.view.LayoutInflater;
-import android.view.Gravity;
-import android.view.WindowManager.LayoutParams;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import java.nio.ByteBuffer;
-import android.widget.LinearLayout;
-import android.os.Handler;
-import android.os.Message;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.text.TextWatcher;
 import android.text.Editable;
 import android.view.inputmethod.EditorInfo;
 import android.view.KeyEvent;
-import android.view.ViewGroup;
 import android.view.TextureView;
 import android.graphics.SurfaceTexture;
 import android.view.Surface;
-import android.os.SystemClock;
 import android.text.InputType;
-import android.util.Log;
+import java.util.Map;
+import java.util.HashMap;
 
 import com.mojang.minecraftpe.TextInputProxyEditTextbox;
 
-public class BoatActivity extends Activity implements View.OnTouchListener, TextureView.SurfaceTextureListener
+public abstract class BoatActivity extends Activity implements View.OnTouchListener, TextureView.SurfaceTextureListener
 {
-    private TextInputProxyEditTextbox textInputWidget;
-    private boolean textBoxShowing = false;
+    private static BoatActivity currentActivity = null;
+
+    public static BoatActivity getCurrentInstance() {
+        return currentActivity;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentActivity = this;
+    }
+
+    public TextInputProxyEditTextbox textInputWidget;
+    public boolean textBoxShowing = false;
 
     public void showKeyboard() {
         BoatActivity.this.textBoxShowing = true;
         BoatActivity.this.textInputWidget.setVisibility(View.VISIBLE);
-        BoatActivity.this.mouseCursor.setVisibility(View.INVISIBLE);
-        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams)new RelativeLayout.LayoutParams(BoatActivity.this.textInputWidget.getLayoutParams());
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) new RelativeLayout.LayoutParams(BoatActivity.this.textInputWidget.getLayoutParams());
         lp.leftMargin = baseX;
         lp.topMargin = baseY;
         BoatActivity.this.textInputWidget.setLayoutParams(lp);
@@ -61,46 +62,22 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
     public void hideKeyboard() {
         BoatActivity.this.textBoxShowing = false;
         BoatActivity.this.textInputWidget.setVisibility(View.GONE);
-        BoatActivity.this.mouseCursor.setVisibility(View.VISIBLE);
     }
+
+    public abstract void findViews();
+
+    public TextureView mainTextureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        base = (RelativeLayout)this.findViewById(R.id.main_base);
-        mainTextureView = (TextureView)this.findViewById(R.id.main_surface);
-        mainTextureView.setSurfaceTextureListener(this);
-        //mainTextureView.setOpaque(false);
-        mainTextureView.setOnTouchListener(this);
-        mouseCursor = (ImageView)findViewById(R.id.mouse_cursor);
-        controlUp = this.findButton(R.id.control_up);
-        controlDown = this.findButton(R.id.control_down);
-        controlLeft = this.findButton(R.id.control_left);
-        controlRight = this.findButton(R.id.control_right);
-        controlJump = this.findButton(R.id.control_jump);
-        controlInv = this.findButton(R.id.control_inventory);
-        controlLshift = this.findButton(R.id.control_lshift);
-        control1 = this.findButton(R.id.control_1);
-        control2 = this.findButton(R.id.control_2);
-        control3 = this.findButton(R.id.control_3);
-        control4 = this.findButton(R.id.control_4);
-        control5 = this.findButton(R.id.control_5);
-        control6 = this.findButton(R.id.control_6);
-        control7 = this.findButton(R.id.control_7);
-        control8 = this.findButton(R.id.control_8);
-        control9 = this.findButton(R.id.control_9);
 
-        itemBar = (LinearLayout)findViewById(R.id.item_bar);
-        mousePrimary = this.findButton(R.id.mouse_primary);
-        mouseSecondary = this.findButton(R.id.mouse_secondary);
-        esc = this.findButton(R.id.esc);
-        showIME = this.findButton(R.id.show_ime);
-        controlDebug = this.findButton(R.id.control_debug);
-        controlCommand = this.findButton(R.id.control_command);
-        control3rd = this.findButton(R.id.control_3rd);
+        findViews();
 
-        textInputWidget = (TextInputProxyEditTextbox)this.findViewById(R.id.text_box);
+        this.mainTextureView.setSurfaceTextureListener(this);
+        //this.mainTextureView.setOpaque(false);
+        this.mainTextureView.setOnTouchListener(this);
+
         this.textInputWidget.setFocusable(true);
         this.textInputWidget.setFocusableInTouchMode(true);
         this.textInputWidget.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
@@ -108,8 +85,6 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
 
         this.textInputWidget.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                 public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                    Log.w("mcpe - keyboard", "onEditorAction: " + actionId);
-                    //BoatActivity.this.nativeReturnKeyPressed();
                     BoatLib.pushEventKey(BoatKeycodes.KEY_ENTER, '\n', true);
                     BoatLib.pushEventKey(BoatKeycodes.KEY_ENTER, '\n', false);
                     return false;
@@ -124,7 +99,6 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
                 }
 
                 public void afterTextChanged(Editable s) {
-                    //MainActivity.this.nativeSetTextboxText(s.toString());
                     String newText = s.toString();
                     if (newText.length() > 0){
                         for(int i = 0; i < newText.length(); i++){
@@ -140,7 +114,6 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
                 public void onDeleteKeyPressed() {
                     BoatActivity.this.runOnUiThread(new Runnable() {
                             public void run() {
-                                //BoatActivity.this.nativeBackSpacePressed();
                                 BoatLib.pushEventKey(BoatKeycodes.KEY_BACKSPACE, 0, true);
                                 BoatLib.pushEventKey(BoatKeycodes.KEY_BACKSPACE, 0, false);
                             }
@@ -150,67 +123,38 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
                 public void onBackKeyPressed() {
                     BoatActivity.this.runOnUiThread(new Runnable() {
                             public void run() {
-                                Log.w("mcpe - keyboard", "textInputWidget.onBackPressed");
-                                //MainActivity.this.nativeBackPressed();
                                 hideKeyboard();
                             }
                         });
                 }
         });
-        mHandler = new MyHandler();
-        base.setSystemUiVisibility(BOAT_SYSTEM_UI_FLAGS);
     }
 
-    public static final int BOAT_SYSTEM_UI_FLAGS =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        base.setSystemUiVisibility(BOAT_SYSTEM_UI_FLAGS);
-    }
+    public int cursorMode = BoatLib.CursorEnabled;
 
     @Override
     public void onBackPressed() {
         BoatLib.pushEventMessage(BoatLib.CloseRequest);
     }
 
+    private BoatTask task;
+
+    @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-        System.out.println("SurfaceTexture is available!");
         BoatLib.setBoatNativeWindow(new Surface(surface));
         BoatLib.setEventPipe();
 
-        final int w = width;
-        final int h = height;
-
-        runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    int scale = 1;
-                    while(w / (scale + 1) >= 320 && h / (scale + 1) >= 240) {
-                        scale++;
-                    }
-                    RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams)itemBar.getLayoutParams();
-                    lp.height = 20 * scale;
-                    lp.width = 20 * scale * 9;
-                    itemBar.setLayoutParams(lp);
-                }
-        });
-
-        new Thread() {
+        Map<String, String> envvars = new HashMap<>();
+        envvars.put(BoatScript.BOAT_ENV_WINDOW_WIDTH, Integer.toString(width));
+        envvars.put(BoatScript.BOAT_ENV_WINDOW_HEIGHT, Integer.toString(height));
+        envvars.put(BoatScript.BOAT_ENV_TMPDIR, this.getCacheDir().getAbsolutePath());
+        this.task = new BoatTask(envvars, getIntent().getStringExtra(BoatTask.BOAT_TASK_SCRIPT_PATH)) {
             @Override
-            public void run() {
-                LoadMe.exec(w, h);
-                Message msg = new Message();
-                msg.what = -1;
-                mHandler.sendMessage(msg);
+            public void afterExecute() {
+                BoatActivity.this.finish();
             }
-        }.start();
+        };
+        this.task.startTask();
     }
 
     @Override
@@ -228,72 +172,8 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
         // TODO: Implement this method
     }
 
-    public void setCursorMode(int mode) { 
-        Message msg = new Message();
-        msg.what = mode;
-        mHandler.sendMessage(msg);
-    }
-
-    private RelativeLayout base;
-    private Button controlUp;
-    private Button controlDown;
-    private Button controlLeft;
-    private Button controlRight;
-    private Button controlJump;
-    private Button controlInv;
-    private Button controlLshift;
-    private Button control1;
-    private Button control2;
-    private Button control3;
-    private Button control4;
-    private Button control5;
-    private Button control6;
-    private Button control7;
-    private Button control8;
-    private Button control9;
-    private LinearLayout itemBar;
-    private Button mousePrimary;
-    private Button mouseSecondary;
-    private Button controlDebug;
-    private Button controlCommand;
-    private Button control3rd;
-    private ImageView mouseCursor;
-    private TextureView mainTextureView;
-    private Button esc;
-    private Button showIME;
-
-    private Button findButton(int id) {
-        Button b = (Button)findViewById(id);
-        b.setOnTouchListener(this);
-        return b;
-    }
-
-    public int cursorMode = BoatLib.CursorEnabled;
-
-    private class MyHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case BoatLib.CursorDisabled:
-                    hideKeyboard();
-                    BoatActivity.this.mouseCursor.setVisibility(View.INVISIBLE);
-                    BoatActivity.this.itemBar.setVisibility(View.VISIBLE);
-                    BoatActivity.this.cursorMode = BoatLib.CursorDisabled;
-                    break;
-                
-                case BoatLib.CursorEnabled:
-                    BoatActivity.this.mouseCursor.setVisibility(View.VISIBLE);
-                    BoatActivity.this.itemBar.setVisibility(View.INVISIBLE);
-                    BoatActivity.this.cursorMode = BoatLib.CursorEnabled;
-                    break;
-                default:
-                    BoatActivity.this.finish();
-                    break;
-            }
-        }
-    }
-
-    private MyHandler mHandler;
+    public abstract void setCursorViewPos(float x, float y);
+    public abstract void setCursorViewMode(int mode);
 
     private int initialX;
     private int initialY;
@@ -301,30 +181,24 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
     private int baseY;
 
     @Override
-    public boolean onTouch(View p1, MotionEvent p2) {
-        if (p1 == showIME) {
-            if (p2.getActionMasked() == MotionEvent.ACTION_UP && this.cursorMode == BoatLib.CursorEnabled) {
-                this.showKeyboard();
-            }
-            return false;
-        }
-        
-        if (p1 == mainTextureView) {
+    public boolean onTouch(View view, MotionEvent event) {
+        if (view == mainTextureView) {
             if (textBoxShowing != false) {
                 hideKeyboard();
             }
+            int currentX = (int)event.getX();
+            int currentY = (int)event.getY();
             if (cursorMode == BoatLib.CursorDisabled) {
-                switch(p2.getActionMasked()) {
+                switch(event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
-                        initialX = (int)p2.getX();
-                        initialY = (int)p2.getY();
+                        initialX = currentX;
+                        initialY = currentY;
                     case MotionEvent.ACTION_MOVE:
-                        BoatLib.pushEventPointer(baseX + (int)p2.getX() -initialX, baseY + (int)p2.getY() - initialY);
+                        BoatLib.pushEventPointer(baseX + currentX -initialX, baseY + currentY - initialY);
                         break;
                     case MotionEvent.ACTION_UP:
-                        baseX += ((int)p2.getX() - initialX);
-                        baseY += ((int)p2.getY() - initialY);
-
+                        baseX += (currentX - initialX);
+                        baseY += (currentY - initialY);
                         BoatLib.pushEventPointer(baseX, baseY);
                         break;
                     default:
@@ -332,110 +206,14 @@ public class BoatActivity extends Activity implements View.OnTouchListener, Text
                 }
             }
             else if (cursorMode == BoatLib.CursorEnabled) {
-                baseX = (int)p2.getX();
-                baseY = (int)p2.getY();
+                baseX = currentX;
+                baseY = currentY;
                 BoatLib.pushEventPointer(baseX, baseY);
             }
-
-            mouseCursor.setX(p2.getX());
-            mouseCursor.setY(p2.getY());
+            setCursorViewPos(event.getX(), event.getY());
             return true;
         }
-        
-        if (p2.getActionMasked() != MotionEvent.ACTION_DOWN && p2.getActionMasked() != MotionEvent.ACTION_UP) {
-            return false;
-        }
-        
-        boolean down = p2.getActionMasked() == MotionEvent.ACTION_DOWN;
-        if (p1 == mousePrimary) {
-            BoatLib.pushEventMouseButton(BoatLib.Button1, down);
-            return false;
-        }
-        if (p1 == mouseSecondary) {
-            BoatLib.pushEventMouseButton(BoatLib.Button3, down);
-            return false;
-        }
-        if (p1 == controlUp) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_W, 0, down);
-            return false;
-        }
-        if (p1 == controlDown) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_S, 0, down);
-            return false;
-        }
-        if (p1 == controlLeft) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_A, 0, down);
-            return false;
-        }
-        if (p1 == controlRight) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_D, 0, down);
-            return false;
-        }
-        if (p1 == controlJump) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_SPACE, 0, down);
-            return false;
-        }
-        if (p1 == esc) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_ESC, 0, down);
-            return false;
-        }
-        if (p1 == controlInv) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_E, 0, down);
-            return false;
-        }
-        if (p1 == controlLshift) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_LEFTSHIFT, 0, down);
-            return false;
-        }
-        if (p1 == control1) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_1, 0, down);
-            return false;
-        }
-        if (p1 == control2) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_2, 0, down);
-            return false;
-        }
-        if (p1 == control3) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_3, 0, down);
-            return false;
-        }
-        if (p1 == control4) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_4, 0, down);
-            return false;
-        }
-        if (p1 == control5) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_5, 0, down);
-            return false;
-        }
-        if (p1 == control6) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_6, 0, down);
-            return false;
-        }
-        if (p1 == control7) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_7, 0, down);
-            return false;
-        }
-        if (p1 == control8) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_8, 0, down);
-            return false;
-        }
-        if (p1 == control9) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_9, 0, down);
-            return false;
-        }
-        if (p1 == controlDebug) {    
-            BoatLib.pushEventKey(BoatKeycodes.KEY_F3, 0, down);
-            return false;
-        }
-        if (p1 == controlCommand) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_SLASH, 0, down);
-            return false;
-        }
-        if (p1 == control3rd) {
-            BoatLib.pushEventKey(BoatKeycodes.KEY_F5, 0, down);
-            return false;
-        }
-        return false; 
-    }   
+        return false;
+    }
 }
 
